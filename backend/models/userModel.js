efd3cc2bfb41979a31193eba809ea0565a41a33d
Manishavelmani <165029909+Manishavelmani@users.noreply.github.com>
2025-02-27 09:28:08 +0530

    Update userModel.js

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -8,13 +8,3 @@ const userSchema=mongoose.Schema({
 
 const userModel=new mongoose.model('users',userSchema)
 module.exports=userModel
-// const mongoose=require('mongoose')
-// const userSchema=mongoose.Schema({
-//     username:String,
-//     email:{type:String,unique:true},
-//     password:String,
-//     isVerified:{type:Boolean,default:false}
-// },{timestamps:true}
-// )
-// const userModel=new mongoose.model('users',userSchema)
-// module.exports=userModel
