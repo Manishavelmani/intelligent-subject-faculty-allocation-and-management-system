32410db04121ec4eab33e14308682b0c9287b5fd
22CS052 <22CS052@PSRENGG.AC.IN>
2025-03-05 12:29:29 +0530

    Removed nested Git repository

---
+++ backend/models/userModel.js
@@ -0,0 +1,20 @@
+const mongoose=require('mongoose')
+const userSchema=mongoose.Schema({
+    username:String,
+    email:String,
+    password:String,
+    degree:String
+})
+
+const userModel=new mongoose.model('users',userSchema)
+module.exports=userModel
+// const mongoose=require('mongoose')
+// const userSchema=mongoose.Schema({
+//     username:String,
+//     email:{type:String,unique:true},
+//     password:String,
+//     isVerified:{type:Boolean,default:false}
+// },{timestamps:true}
+// )
+// const userModel=new mongoose.model('users',userSchema)
+// module.exports=userModel
